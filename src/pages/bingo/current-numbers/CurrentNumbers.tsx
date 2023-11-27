0e5ed4de4def26c50ef0b0b739fc219db602f107
HunterECampbell <hcnureth@gmail.com>
2023-11-27 10:49:31 -0700

    Remove commented code

--- src/pages/bingo/current-numbers/CurrentNumbers.tsx
+++ src/pages/bingo/current-numbers/CurrentNumbers.tsx
@@ -1,18 +1,6 @@
-// import { useEffect, useState } from "react";
-
-// import AnimatedView from "@/pages/bingo/current-numbers/AnimatedView";
-// import DefaultView from "@/pages/bingo/current-numbers/DefaultView";
-
 const CurrentNumbers = ({ currentNums }: { currentNums: [string, string] }) => {
-  // const [isAnimating, setIsAnimating] = useState(false);
-
   return (
     <div className="flex justify-center items-center">
-      {/* {!isAnimating ? (
-        <DefaultView carouselNums={carouselNums} />
-      ) : (
-        <AnimatedView carouselNums={carouselNums} />
-      )} */}
       <div
         id="small-num"
         className="raised-connected font-bold rounded-lg h-12 w-12 mx-3 flex justify-center items-center"
