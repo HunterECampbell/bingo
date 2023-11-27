8f2ed735d023dc471272df516c27eaa50432d3eb
HunterECampbell <hcnureth@gmail.com>
2023-11-27 12:29:30 -0700

    Updates

--- prerender-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"99ad327f37a2b1feb01cbf51f67e87d0\",\"previewModeSigningKey\":\"29a62cfbf6d2afea70cc90a71f78711703e7da10b1aefccca54691bc8f584cc5\",\"previewModeEncryptionKey\":\"aedb984f6736ea14f43c90d927a9f3dee5bf5ba0d53233819ac94977fd0b9c99\"},\"notFoundRoutes\":[]}"
